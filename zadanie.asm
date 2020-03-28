a7efc08161ae2873acafd9cf6b707702b83d6b66
asledz <anita.gaja.sledz@gmail.com>
2020-03-28 19:41:47 +0100

    Sprawdzam ilosc argumentow

--- zadanie.asm
+++ zadanie.asm
@@ -1,15 +1,26 @@
 
-; ENDING WITH CODE
-
+; ENDING WITH CODE MACRO
 %macro end_with_code 1
   mov     eax, 1
   mov     ebx, %1
   int     0x80
 %endmacro
 
+
+
 global _start
 
 section .rodata
 
 _start: 
-  end_with_code 4
+  ; pop number of arguments
+  pop rax
+  cmp rax, 5
+  jne .wrongInput
+
+
+
+  end_with_code 0
+
+  .wrongInput
+    end_with_code 1
