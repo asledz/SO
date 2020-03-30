50e48ec5d505b550432322672c677c8b5ea4e91c
asledz <anita.gaja.sledz@gmail.com>
2020-03-30 02:14:24 +0200

    eh

--- dcl.asm
+++ dcl.asm
@@ -151,7 +151,7 @@ TOP_LIMIT       equ 90
     correct_character           r14b
 
     code_letter_with_Q          r13b
-;    code_letter_with_rotor      r9
+    code_letter_with_rotor      r9
     code_letter_with_Q_reverse  r13b
 ;
     code_letter_with_Q          r12b
