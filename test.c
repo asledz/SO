77d0cd148c9bcfe3c57d12e76abb4d84a62668af
asledz <anita.gaja.sledz@gmail.com>
2020-04-09 20:15:40 +0200

    snalkjdhakjlh

--- test.c
+++ test.c
@@ -4,13 +4,13 @@
 #include "pix.h"
 
 int main(){
-	uint64_t n = 1000;
+	uint64_t n = 8;
 	uint32_t* tab = malloc(n * sizeof(uint32_t));
 	uint64_t index = 0;
 
 	pix(tab, &index, n);
 
-	// for (uint64_t i = 0; i < n; ++i) {
-	// 	printf("%08x\n", tab[i]);
-	// }
+	 for (uint64_t i = 0; i < n; ++i) {
+	 	printf("%08x\n", tab[i]);
+	 }
 }
